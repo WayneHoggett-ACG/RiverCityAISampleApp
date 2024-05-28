ece0f60ec057933ac8d7f36971c7c61bd014e5bb
Wayne Hoggett <whoggett@auscript.com.au>
2024-05-29 09:15:41 +1000

    Added Vnet Integration Variable

--- RiverCityAI/Pages/Index.cshtml.cs
+++ RiverCityAI/Pages/Index.cshtml.cs
@@ -8,6 +8,7 @@ public class IndexModel : PageModel
     private readonly ILogger<IndexModel> _logger;
 
     public string ServerName { get; private set; }
+    public string ServerPrivateIP { get; private set; }
 
     public IndexModel(ILogger<IndexModel> logger)
     {
@@ -17,5 +18,13 @@ public class IndexModel : PageModel
     public void OnGet()
     {
         ServerName = Environment.MachineName;
+        try
+        {
+            ServerPrivateIP = Environment.GetEnvironmentVariable("WEBSITE_PRIVATE_IP").ToString();
+        }
+        catch
+        {
+            ServerPrivateIP = "Not VNet Integrated";
+        }
     }
 }
